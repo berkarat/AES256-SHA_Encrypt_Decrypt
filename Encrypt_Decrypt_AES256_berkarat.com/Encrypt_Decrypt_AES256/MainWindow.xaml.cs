d6ba0ea59368f924c53c71612fd762d48c9a39f4
Berk Arat <berkarat93@hotmail.com>
2018-10-22 22:30:49 +0300

    AES256-SHA_Encrypt_Decrypt
    AES 256 Şifreleme ve Deşifreleme

---
+++ Encrypt_Decrypt_AES256_berkarat.com/Encrypt_Decrypt_AES256/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Encrypt_Decrypt_AES256
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow ()
+        {
+            InitializeComponent ();
+            textBox.MaxLength=16;
+            textBox1.MaxLength=32;
+           
+        }
+
+        private void button_Click (object sender, RoutedEventArgs e)
+        {
+            richTextBox.Document.Blocks.Clear ();
+            byte[] IV = Encoding.UTF8.GetBytes (textBox.Text);
+            byte[] KEY = Encoding.UTF8.GetBytes (textBox1.Text);
+            if (textBox.Text.Length==16 && textBox1.Text.Length==32)
+            {
+                richTextBox.AppendText (Methods.EncryptString (textBox2.Text, KEY, IV));
+            }
+            else
+            {
+                MessageBox.Show ("KARAKTER SINIRI");
+            }
+
+           
+        }
+    }
+}
---
+++ Encrypt_Decrypt_AES256_berkarat.com/Encrypt_Decrypt_AES256/Methods.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encrypt_Decrypt_AES256
+{
+    public static class Methods
+    {
+
+
+
+
+        public static string EncryptString (string plainText, byte[] key, byte[] iv)
+        {
+
+            Aes encryptor = Aes.Create ();
+
+            encryptor.Mode=CipherMode.CBC;
+            //   encryptor.KeySize = 256;
+            //encryptor.BlockSize = 128;
+            //  encryptor.Padding = PaddingMode.Zeros;
+
+            // Set key and IV
+            encryptor.Key=key;
+            encryptor.IV=iv;
+
+
+            MemoryStream memoryStream = new MemoryStream ();
+
+
+            ICryptoTransform aesEncryptor = encryptor.CreateEncryptor ();
+
+            CryptoStream cryptoStream = new CryptoStream (memoryStream, aesEncryptor, CryptoStreamMode.Write);
+
+            byte[] plainBytes = Encoding.ASCII.GetBytes (plainText);
+
+            cryptoStream.Write (plainBytes, 0, plainBytes.Length);
+
+            cryptoStream.FlushFinalBlock ();
+
+            byte[] cipherBytes = memoryStream.ToArray ();
+
+            memoryStream.Close ();
+            cryptoStream.Close ();
+
+            string cipherText = Convert.ToBase64String (cipherBytes, 0, cipherBytes.Length);
+
+            return cipherText;
+        }
+
+        public static string DecryptString (string cipherText, byte[] key, byte[] iv)
+        {
+
+            Aes encryptor = Aes.Create ();
+
+            encryptor.Mode=CipherMode.CBC;
+            // encryptor.KeySize = 256;
+            //encryptor.BlockSize = 128;
+            //  encryptor.Padding = PaddingMode.Zeros;
+
+            // Set key and IV
+            encryptor.Key=key;
+            encryptor.IV=iv;
+
+
+            MemoryStream memoryStream = new MemoryStream ();
+
+            ICryptoTransform aesDecryptor = encryptor.CreateDecryptor ();
+
+            CryptoStream cryptoStream = new CryptoStream (memoryStream, aesDecryptor, CryptoStreamMode.Write);
+
+            string plainText = String.Empty;
+
+            try
+            {
+                byte[] cipherBytes = Convert.FromBase64String (cipherText);
+
+                cryptoStream.Write (cipherBytes, 0, cipherBytes.Length);
+
+                cryptoStream.FlushFinalBlock ();
+
+                byte[] plainBytes = memoryStream.ToArray ();
+
+                plainText=Encoding.ASCII.GetString (plainBytes, 0, plainBytes.Length);
+            }
+            finally
+            {
+                memoryStream.Close ();
+                cryptoStream.Close ();
+            }
+
+            // Return the decrypted data as a string
+            return plainText;
+        }
+        public static string CreateSignature (string clearpwd, int x)
+        {
+            using (var sha = SHA256.Create ())
+            {
+                var computedHash = sha.ComputeHash (Encoding.Unicode.GetBytes (clearpwd+RandomString (x)));
+                return Convert.ToBase64String (computedHash);
+            }
+        }
+        private static Random random = new Random ();
+        public static string RandomString (int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string (Enumerable.Repeat (chars, length)
+              .Select (s => s[random.Next (s.Length)]).ToArray ());
+        }
+
+
+
+
+
+    }
+}
